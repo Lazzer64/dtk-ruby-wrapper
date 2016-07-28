fd61fa9bdee6d7892dfea4da74679272d0661557
Alex Pelavin <ajpele@gmail.com>
2016-07-28 19:27:13 +0000

    Implemented parsing of json

--- lib/wrapper.rb
+++ lib/wrapper.rb
@@ -1,19 +1,30 @@
 #!/usr/bin/ruby
+require 'json'
 
 def run(entry_point, properties, outputs, requirements)
   requirements.each do |requirement|
     require_relative(requirement)
   end
 
-  obj = Object.const_get(entry_point).new(properties)
-  resp = obj.create
+  klass = Object.const_get(entry_point.split('.')[0])
+  funct = entry_point.split('.')[1]
+
+  resp = klass.send(funct, properties)
 
   ret = {}
   outputs.each do |output|
-    ret[output] = resp[output]
+    if resp.key?(output)
+      ret[output] = resp[output]
+    else
+      ret[output.to_sym] = resp[output.to_sym]
+    end
   end
   puts ret
+  exit(0)
+rescue => e
+  STDERR.puts e.to_s
+  exit(1)
 end
 
-opts = { region: 'us-west-2', stream_name: 'astreamname', shard_count: 1 }
-run('Resource::Kinesis::Stream', opts, [:stream_arn], ['../../resource/lib/resource', '../../kinesis/lib/kinesis'])
+p = JSON.parse(ARGV[0])
+run(p['entry_point'], p['properties'], p['outputs'], p['requirements'])
